edadea13b2b63aa7e9f9649331f0879de0675788
Vojtech Soban <vojtech.soban@citrix.com>
2021-09-09 06:31:36 +0200

    Initial commit

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,33 +1,116 @@
-import { expect } from 'chai'
-import * as calc from '../src'
+import {expect} from 'chai'
+import {accepted, badRequest, created, HttpResponse, noContent, ok, withHttpResponse} from '../src'
 
-describe('calc tests', () => {
-	describe('sum()', () => {
-		context('when it receives "a" = 2 and "b" = 1', () => {
-			it('should return 3', () => {
-				expect(calc.sum(2, 1)).to.equal(3)
-			})
-		})
-	})
-	describe('sub()', () => {
-		context('when it receives "a" = 2 and "b" = 1', () => {
-			it('should return 1', () => {
-				expect(calc.sub(2, 1)).to.equal(1)
-			})
-		})
-	})
-	describe('div()', () => {
-		context('when it receives "a" = 4 and "b" = 2', () => {
-			it('should return 2', () => {
-				expect(calc.sub(4, 2)).to.equal(2)
-			})
-		})
-	})
-	describe('mult()', () => {
-		context('when it receives "a" = 2 and "b" = 3', () => {
-			it('should return 6', () => {
-				expect(calc.mult(2, 3)).to.equal(6)
-			})
-		})
-	})
+describe('HttpResponse', () => {
+    describe('HTTP headers', () => {
+        [
+            [
+                'convert HTTP header to array',
+                {'Content-Type': 'application/json'},
+                [{
+                    name: 'Content-Type',
+                    value: 'application/json'
+                }]
+            ],
+            [
+                'take HTTP headers as array',
+                [{
+                    name: 'Content-Type',
+                    value: 'application/json'
+                }],
+                [{name: 'Content-Type', value: 'application/json'}]
+            ],
+            [
+                'no HTTP headers',
+                undefined,
+                []
+            ],
+            [
+                'empty HTTP headers array',
+                [],
+                []
+            ],
+            [
+                'empty HTTP headers object',
+                {},
+                []
+            ]
+        ]
+            .forEach(([name, headers, expected]: any[]) => {
+                it(name, () => {
+                    const response = new HttpResponse(201, headers, '{"hello": "world"}')
+                    expect(response.headers).to.be.deep.equal(expected)
+                })
+            })
+    })
+    describe('statusCode', () => {
+        it('default value is 200', () => {
+            expect(new HttpResponse().statusCode).to.be.equal(200)
+        })
+        it('positional argument', () => {
+            expect(new HttpResponse(222).statusCode).to.be.equal(222)
+        })
+        it('named argument', () => {
+            expect(new HttpResponse({statusCode: 333}).statusCode).to.be.equal(333)
+        })
+    })
+    describe('body', () => {
+        it('default value is empty string', () => {
+            expect(new HttpResponse().body).to.be.equal('')
+        })
+        it('positional argument', () => {
+            expect(new HttpResponse(200, undefined, 'hello').body).to.be.equal('aGVsbG8=')
+        })
+        it('named argument', () => {
+            expect(new HttpResponse({body: 'world'}).body).to.be.equal('d29ybGQ=')
+        })
+    })
+    describe('Factory methods', () => {
+
+        function test(builder, body = undefined) {
+            const fn = withHttpResponse(builder)
+            return fn(body)
+        }
+
+        it('ok', () => {
+            expect(test(ok, 'body')).to.be.deep.equal({
+                "body": "Ym9keQ==",
+                "headers": [],
+                "statusCode": 200,
+            })
+        })
+        it('created', () => {
+            expect(test(created, 'body')).to.be.deep.equal({
+                "body": "Ym9keQ==",
+                "headers": [],
+                "statusCode": 201,
+            })
+        })
+        it('noContent', () => {
+            expect(test(noContent)).to.be.deep.equal({
+                "body": "",
+                "headers": [],
+                "statusCode": 204,
+            })
+        })
+        it('accepted', () => {
+            expect(test(accepted, 'body')).to.be.deep.equal({
+                "body": "Ym9keQ==",
+                "headers": [],
+                "statusCode": 202,
+            })
+        })
+        it('badRequest', () => {
+            expect(test(badRequest, 'body')).to.be.deep.equal({
+                "body": "Ym9keQ==",
+                "headers": [],
+                "statusCode": 400,
+            })
+        })
+    })
+    describe('test JSON serialization', () => {
+        it('HTTP ok with content', () => {
+            expect(JSON.stringify(ok('body'))).to.be.equal('{"statusCode":200,"headers":[],"body":"Ym9keQ=="}')
+        })
+    })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,154 @@
-export const sum = (a: number, b: number): number => a + b
-export const sub = (a: number, b: number): number => a - b
-export const div = (a: number, b: number): number => a / b
-export const mult = (a: number, b: number): number => a * b
+import {encodeContent} from 'inp-commons'
+
+const applicationJsonHeader = {'content-type': 'application/json'}
+
+interface InPHeader {
+	name: string,
+	value: string,
+}
+
+export class HttpResponse {
+
+    private static encode(content) {
+        return encodeContent(content)
+    }
+
+    private static convertHeaders(inputHeaders): InPHeader[] {
+        if (Array.isArray(inputHeaders)) {
+            return inputHeaders
+        } else {
+            return Object.entries(inputHeaders).map(([name, value]) => ({name, value})) as InPHeader[]
+        }
+    }
+
+	  public statusCode: number
+	  public headers: any[]
+	  public body: string
+
+
+    /**
+     *
+     * @param args statusCode, headers, body or object {statusCode, headers, body}
+		 */
+		public constructor(...args) {
+        let statusCode, headers, body
+        if (typeof args[0] === 'object') {
+            ({statusCode, headers, body} = args[0])
+        } else {
+            ([statusCode, headers, body] = args)
+        }
+        this.statusCode = statusCode || 200
+        this.headers = HttpResponse.convertHeaders(headers || {})
+        this.body = HttpResponse.encode(body || '')
+    }
+}
+
+export function ok(body, headers = {}) {
+    return new HttpResponse({statusCode: 200, body, headers})
+}
+
+export function created(body, headers = {}) {
+    return new HttpResponse({statusCode: 201, body, headers})
+}
+
+export function accepted(body, headers = {}) {
+    return new HttpResponse({statusCode: 202, body, headers})
+}
+
+export function noContent(headers = {}) {
+    return new HttpResponse({statusCode: 204, headers})
+}
+
+export function badRequest(body) {
+    return new HttpResponse({statusCode: 400, body})
+}
+
+export function unauthorized(body) {
+    return new HttpResponse({statusCode: 401, body})
+}
+
+export function internalError(body) {
+    return new HttpResponse({statusCode: 500, body})
+}
+
+export function apiError(type, detail, parameters, statusCode = 400) {
+    return new HttpResponse({statusCode, body: JSON.stringify({type, detail, parameters})})
+}
+
+export function processUnauthorizedResponse(response) {
+	if (response.status === 401) {
+		return response.text()
+			// FIXME copy headers? content-type etc?
+			.then(unauthorized)
+			.then(error => {
+				throw error
+			})
+	} else {
+		return response
+	}
+}
+
+function buildSuccessResponse(result) {
+    if (result instanceof HttpResponse) {
+        return result
+    } else {
+        return new HttpResponse({body: result, statusCode: 200})
+    }
+}
+
+function buildErrorFromCaughtError(error) {
+	if (error == null) {
+		return {message: 'Caught unexpected error with no context'}
+	} else if (typeof error === 'string') {
+		return {message: error}
+	} else if (error instanceof Error || error.message || error.stack) {
+		if (process?.env?.NODE_ENV === 'development' && error.stack) {
+			return ({message: error.message, exception: `${error.message}\n${error.stack}`})
+		} else {
+			return ({message: error.message})
+		}
+	} else {
+		// Handling caught Graal proxy object
+		const body = JSON.stringify(error)
+		if (body === '{}') {
+			return {message: String(error)}
+		} else {
+			return {message: body}
+		}
+	}
+}
+
+function buildApiErrorFromCaughtError(error) {
+	const {message, exception} = buildErrorFromCaughtError(error)
+	return {type: 'https://errors-api.cloud.com/integration-script/unexpected-error', detail: message, exception}
+}
+
+function handleUnexpectedError(error, statusCode = 500) {
+    if (error instanceof HttpResponse) {
+        return error
+    }
+    return new HttpResponse(statusCode, applicationJsonHeader, JSON.stringify(buildApiErrorFromCaughtError(error)))
+}
+
+export function withHttpResponse(fn) {
+	return (...args) => {
+		try {
+			const result = fn(...args)
+			if (result instanceof HttpResponse) {
+				return result
+			} else if (result instanceof Promise) {
+				return result
+					.then(buildSuccessResponse)
+					.catch(error => {
+						console.error(`Caught unexpected error in withHttpResponse wrapper: ${error instanceof String} ${error instanceof Error} ${error} ${buildApiErrorFromCaughtError(error)}`)
+						return handleUnexpectedError(error)
+					})
+			} else {
+				return Promise.resolve(buildSuccessResponse(result))
+			}
+		} catch (error) {
+			console.error(`Caught unexpected error in withHttpResponse wrapper: ${buildApiErrorFromCaughtError(error)}`)
+			return Promise.resolve(handleUnexpectedError(error))
+		}
+	}
+}
